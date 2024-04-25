cbb6d7da183aa201a8c2e867fc13c0428bb63eb7
Haytham Mohamed <haybu@hotmail.com>
2024-04-24 23:53:23 -0500

    tables extracted is ok

--- src/main/java/io/agilehandy/demoreadmsword/MSWordReader.java
+++ src/main/java/io/agilehandy/demoreadmsword/MSWordReader.java
@@ -16,6 +16,9 @@ import java.util.regex.Pattern;
 
 
 /**
+ * Haytham Mohamed
+ * 4/15/2024
+ *
  * observation:
  *  sections and subsections and answers are on cells
  *  title and questions are in paragraph
@@ -97,8 +100,16 @@ public class MSWordReader {
 
     // section, subSection, subSectionDescription, subSubSection, SubSubSubSection, answer
     private void readTable(XWPFTable table) {
-        System.out.println("=== Table Content ===");
-        String txt = this.getTableContent(table);
+        //System.out.println("=== Table Content ===");
+        String txt = this.getTableContent(table).toString();
+        //if (!nestedTables.isEmpty()) System.out.println("there are tables inside main tables");//////////
+        //txt = this.trimLastCharacter(txt, CELL_SEPARATOR);
+        int totalNumberOfTables = this.getNumberOfNestedTables(table);
+        System.out.println("Number of tables: " + totalNumberOfTables);
+        if (totalNumberOfTables == 1 && (table.getNumberOfRows() == 1 || !isAlignedHeadersToRows(txt))) {
+            txt = this.clearTags(txt).trim();
+        }
+        System.out.println(txt);
         this.setLabelForRow(txt);
         if (label == PartLabels.SECTION) {
             section = txt.trim();
@@ -122,117 +133,118 @@ public class MSWordReader {
         }
     }
 
-    private String getTableContent(XWPFTable table) {
-        System.out.println("=== Table ===");
-        //System.out.println("number of rows in this table before clearing nested ones = " + table.getNumberOfRows());
-        XWPFTable contentTable = getNestedTable(table);
-        //System.out.println("number of rows in this table after clearing nested ones = " + contentTable.getNumberOfRows());
-        List<XWPFTableRow> rows = contentTable.getRows();
+    private StringBuffer getTableContent(XWPFTable table) {
+        //System.out.println("=== Table ===");
+        List<XWPFTableRow> rows = table.getRows();
         StringBuffer rowBuffer;
         StringBuffer tableBuffer = new StringBuffer();
-        int numberOfRows = 0;
         for (XWPFTableRow row : rows) {
-            numberOfRows++;
             rowBuffer = this.getRowContent(row);
-            if (numberOfRows == 1) {
-                String temp = rowBuffer.toString();
-                temp = temp.replaceAll("<td>", "<th>")
-                        .replaceAll("</td>", "</th>").trim();
-                rowBuffer = new StringBuffer(temp);
-            }
             tableBuffer.append(rowBuffer);
             //System.out.println(rowBuffer.toString());/////
         }
         String txt = tableBuffer.toString().trim();
-        System.out.println(txt);//////////
-        //txt = this.trimLastCharacter(txt, CELL_SEPARATOR);
-        if (numberOfRows == 1 || !isAlignedHeadersToRows(txt)) {
-            txt = this.clearTags(txt).trim();
-        } else {
-            txt = "<table>" + txt.trim() + "</table>";
+        if (!StringUtils.isEmpty(txt)) {
+            tableBuffer.delete(0, tableBuffer.length());
+            tableBuffer.append("<table>" + txt.trim() + "</table>");
         }
-        return txt;
+        /**
+        if (!StringUtils.isEmpty(txt) && (table.getNumberOfRows() == 1 || !isAlignedHeadersToRows(txt))) {
+            txt = this.clearTags(tableBuffer.toString()).trim();
+            tableBuffer.delete(0, tableBuffer.length());
+            tableBuffer.append(txt);
+        }
+        /**
+        if (table.getNumberOfRows() >= 1) {
+            String temp = tableBuffer.toString();
+            temp = temp.replaceAll("<td>", "<th>")
+                    .replaceAll("</td>", "</th>").trim();
+            tableBuffer.delete(0, tableBuffer.length());
+            tableBuffer.append(temp);
+        }
+         */
+
+        //System.out.println(tableBuffer);//////////
+        return tableBuffer;
+    }
+
+    private StringBuffer getRowContent(XWPFTableRow row) {
+        //System.out.println("=== Table Row ===");
+        StringBuffer rowTextBuffer = new StringBuffer();
+        List<XWPFTableCell> cells = row.getTableCells();
+        int cellCount = 0;
+        String temp;
+        for (XWPFTableCell cell : cells) {
+            rowTextBuffer.append(this.getCellContent(cell));
+            //System.out.println("=== Cell ===");
+            //System.out.println(rowTextBuffer);
+        }
+        if (!rowTextBuffer.isEmpty()) {   // && rowTextBuffer.toString().contains("<td>")
+            temp = rowTextBuffer.toString();
+            rowTextBuffer.delete(0, rowTextBuffer.length());
+            rowTextBuffer.append("<tr>" + temp.trim() + "</tr>");
+        }
+        //System.out.println(rowTextBuffer);
+        return rowTextBuffer;
+    }
+
+    private StringBuffer getCellContent(XWPFTableCell cell) {
+        //System.out.println("Reading Table Cell");
+        //System.out.println("Cell type: " + cell.getBodyElements().get(0).getElementType().name());/////
+        StringBuffer cellContent = new StringBuffer();
+
+        if (cell.getBodyElements().get(0).getElementType() == BodyElementType.PARAGRAPH) {
+            String txt = cell.getTextRecursively().trim();
+            if (!StringUtils.isEmpty(txt))
+                cellContent.append("<td>" + txt + "</td>");
+            else
+                cellContent.append("<td></td>");
+        } else if (cell.getBodyElements().get(0).getElementType() == BodyElementType.TABLE) { // nested table in a cell
+            //System.out.println("nested table inside a cell is found");
+            List<XWPFTable> nestedTables = cell.getTables();
+            StringBuffer nestedBuffer;
+            for (XWPFTable nestedTable : nestedTables) {
+                nestedBuffer = this.getTableContent(nestedTable);
+                if (!StringUtils.isEmpty(nestedBuffer.toString()))
+                    cellContent.append("<td>" + nestedBuffer + "</td>");
+                else
+                    cellContent.append("<td></td>");
+            }
+        }
+        return cellContent;
+    }
+
+    private boolean tableStringWithMoreRows(String txt) {
+        return !StringUtils.isEmpty(txt) && (txt.split("</tr>").length > 1);
     }
 
     private boolean isAlignedHeadersToRows(String txt) {
         String[] rows = txt.split("</tr>");
         if (rows.length > 1) {
-            String[] headers = rows[0].split("</th>");
+            String[] headers = rows[0].split("</td>");
             String[] rowCells = rows[1].split("</td>");
             System.out.println("there are " + headers.length + " headers and " + rowCells.length + " data cells");
             return headers.length == rowCells.length;
         } else
-            return true;
+            return false;
     }
 
     private XWPFTable getNestedTable(XWPFTable table) {
         if(table.getNumberOfRows() == 1) {
             List<XWPFTable> nestedTables = table.getRow(0).getCell(0).getTables();
             if (!nestedTables.isEmpty()) {
-                System.out.println("Nested table found");
+                //System.out.println("Nested table found");
                 return nestedTables.get(0);
             }
             else {
-                System.out.println("Nested table is NOT found");
+                //System.out.println("Nested table is NOT found");
                 return table;
             }
         }
         else {
-            System.out.println("table has more than 1 row, no Nested table to find");
+            //System.out.println("table has more than 1 row, no Nested table to find");
             return table;
         }
-    }
-
-    private StringBuffer getRowContent(XWPFTableRow row) {
-        System.out.println("=== Table Row ===");
-        StringBuffer rowTextBuffer = new StringBuffer();
-        List<XWPFTableCell> cells = row.getTableCells();
-        String cellContent;
-        int cellCount = 0;
-        String temp;
-        for (XWPFTableCell cell : cells) {
-            cellCount++;
-            cellContent = this.getCellContent(cell);
-
-            if (!StringUtils.isEmpty(cellContent)) {
-                rowTextBuffer.append("<td>" + cellContent.trim() + "</td>");
-            } else {
-                rowTextBuffer.append("<td></td>");
-            }
-
-            /**
-            if (cellCount == 0 && !StringUtils.isEmpty(cellContent)) { // first cell
-                rowTextBuffer.append(cellContent.trim());
-            } else if (cellCount == 1 && !StringUtils.isEmpty(cellContent)) {
-                temp = rowTextBuffer.toString();
-                rowTextBuffer = new StringBuffer("<td>" + temp.trim() + "</td>");
-                rowTextBuffer.append("<td>" + cellContent.trim() + "</td>");
-            } else if (cellCount > 1 && !StringUtils.isEmpty(cellContent)) {
-                rowTextBuffer.append("<td>" + cellContent.trim() + "</td>");
-            }
-             */
-            System.out.println("=== Cell ===");
-            System.out.println(rowTextBuffer);
-        }
-        if (!rowTextBuffer.isEmpty() && rowTextBuffer.toString().contains("<td>")) {
-            temp = rowTextBuffer.toString();
-            rowTextBuffer = new StringBuffer("<tr>" + temp + "</tr>");
-        }
-        //System.out.println(rowTextBuffer);
-        return rowTextBuffer;
-    }
-
-    private String getCellContent(XWPFTableCell cell) {
-        System.out.println("Reading Table Cell");
-        String txt = cell.getTextRecursively();
-        System.out.println("Cell type: " + cell.getBodyElements().get(0).getElementType().name());/////
-
-        //String txt = cell.getText();
-        if (!StringUtils.isEmpty(txt)) {
-            //System.out.println(txt.trim());
-            return txt.trim();
-        }
-        return "";
     }
 
     private String clearTags(String txt) {
@@ -243,6 +255,8 @@ public class MSWordReader {
                 .replaceAll("</td>", "")
                 .replaceAll("<th>", "")
                 .replaceAll("</th>", "")
+                .replaceAll("<table>","")
+                .replaceAll("</table>","")
                 .trim();
     }
 
@@ -338,6 +352,22 @@ public class MSWordReader {
             label = PartLabels.NOT_VALID;
         }
         //System.out.println("label: " + label.name());
+    }
+
+    private int getNumberOfNestedTables(XWPFTable table) {
+        for (XWPFTableRow row : table.getRows()) {
+            for (XWPFTableCell cell : row.getTableCells()) {
+                List<XWPFTable> list = cell.getTables();
+                if (list.isEmpty()) {
+                    return 1;
+                } else {
+                    for (XWPFTable t : list) {
+                        return 1 + getNumberOfNestedTables(t);
+                    }
+                }
+            }
+        }
+        return 1;
     }
 
     private void resetSections() {
